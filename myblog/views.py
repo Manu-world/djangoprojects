7ae3ab42cee63e4c23bfacf83b918f7ec6610eea
manu <manu.softwareengineer@gmail.com>
2024-01-26 17:23:10 +0000

    created model

--- myblog/views.py
+++ myblog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Todo
+from .forms import TodoForm
 
 # Create your views here.
+
+def listView(request):
+    template ='index.html'
+    context = {}
+
+    todos = Todo.objects.all
+
+    context['todos']= todos
+
+    return render(request, template, context)
